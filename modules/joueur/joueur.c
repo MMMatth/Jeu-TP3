f17c52377ceab493f1c21789f69829433ddafc7d
MMMatth <mat.loup04@gmail.com>
2023-03-02 17:29:53 +0100

    version main propre

--- modules/joueur/joueur.c
+++ modules/joueur/joueur.c
@@ -1,37 +1,5 @@
-#include "joueur.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
-int deplacement(struct joueur_t *j) {
-    char c;
-    while (1) {
-        c = getch();
-        if (c=='z' || c=='q' || c=='s' || c=='d'){
-            printf("%c\n", c);
-            if (c=='z') j->pos[1] -= 1;
-            if (c=='q') j->pos[0] -= 1;
-            if (c=='s') j->pos[1] += 1;
-            if (c=='d') j->pos[0] += 1;
-            printf("%d %d", j->pos[0], j->pos[1]);
-            }
-        else{
-            printf("touche '%c' non definie", c);
-        if (c == '!') break;
-    }
-    return 0;
-}}
+#include "joueur.h"
 
-void CreationJoueur(struct joueur_t *j){
-    j->pos[0] = 0;
-    j->pos[1] = 0;
-}
-
-
-int main() {
-    struct joueur_t j;
-    j.pos[0] = 0;
-    j.pos[1] = 0;
-    deplacement(&j);
-    return 0;
-}
--- modules/joueur/joueur.h
+++ modules/joueur/joueur.h
@@ -1,9 +1,3 @@
-/**
- * \file player.h
- * \brief fichier d'en-tête du module joueur
- * 
- */
-
 #ifndef PLAYER_H
 #define PLAYER_H
 
@@ -12,12 +6,6 @@ struct joueur {
 };
 typedef struct joueur joueur_t;
 
-/**
- * \brief procedure crée un joueur. Vous aurez besoin de malloc(sizeof(struct
- *   joueur_t)). Vous pouvez ajouter les arguments dont vous avez besoin à cette fonction.
- * 
- * 
- */
 void creationJoueur();
 
 char * toStringJr(struct joueur_t *j);
