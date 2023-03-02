d15878bd50ad18772a203a9cc22c61ea5611e345
gabinxc <gabinxcln@gmail.com>
2023-03-02 17:31:34 +0100

    deplacements termines

--- modules/joueur/joueur.c
+++ modules/joueur/joueur.c
@@ -5,20 +5,19 @@
 
 int deplacement(struct joueur_t *j) {
     char c;
-    while (1) {
-        c = getch();
-        if (c=='z' || c=='q' || c=='s' || c=='d'){
-            printf("%c\n", c);
-            if (c=='z') j->pos[1] -= 1;
-            if (c=='q') j->pos[0] -= 1;
-            if (c=='s') j->pos[1] += 1;
-            if (c=='d') j->pos[0] += 1;
-            printf("%d %d", j->pos[0], j->pos[1]);
-            }
-        else{
-            printf("touche '%c' non definie", c);
-        if (c == '!') break;
-    }
+    c = getch();
+    if (c=='z' || c=='q' || c=='s' || c=='d' || c=='!'){
+        printf("%c\n", c);
+        //si touche p, on quitte le jeu
+        if (c=='!') exit(0);
+        if (c=='z') j->pos[1] -= 1;
+        if (c=='q') j->pos[0] -= 1;
+        if (c=='s') j->pos[1] += 1;
+        if (c=='d') j->pos[0] += 1;
+        printf("%d %d\n", j->pos[0], j->pos[1]);
+        }
+    else{
+        printf("touche '%c' non definie", c);
     return 0;
 }}
 
@@ -27,11 +26,11 @@ void CreationJoueur(struct joueur_t *j){
     j->pos[1] = 0;
 }
 
-
 int main() {
     struct joueur_t j;
-    j.pos[0] = 0;
-    j.pos[1] = 0;
-    deplacement(&j);
+    CreationJoueur(&j);
+    while (1) {
+        deplacement(&j);
+    }
     return 0;
 }
